9ee906a648fa11bb621c3de9683954053fac1776
M-racle <108946729+M-racle@users.noreply.github.com>
2025-01-09 17:40:42 +0300

    Created interfaces (which will hold signatures) and classes for AutoMapper and Category services

---
+++ Services/Catalog/MDShop.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using MDShop.Catalog.Dtos.CategoryDtos;
+using MDShop.Catalog.Dtos.ProductDetailDtos;
+using MDShop.Catalog.Dtos.ProductDtos;
+using MDShop.Catalog.Dtos.ProductImageDtos;
+using MDShop.Catalog.Entities;
+
+namespace MDShop.Catalog.Mapping {
+    public class GeneralMapping : Profile {
+        public GeneralMapping() {
+
+            CreateMap<Category,ResultCategoryDto>().ReverseMap();
+            CreateMap<Category, CreateCategoryDto>().ReverseMap();
+            CreateMap<Category, UpdateCategoryDto>().ReverseMap();
+            CreateMap<Category, GetByIdCategoryDto>().ReverseMap();
+
+            CreateMap<Product, ResultProductDto>().ReverseMap();
+            CreateMap<Product, CreateProductDto>().ReverseMap();
+            CreateMap<Product, UpdateProductDto>().ReverseMap();
+            CreateMap<Product, GetByIdProductDto>().ReverseMap();
+
+            CreateMap<ProductDetail, ResultProductDetailDto>().ReverseMap();
+            CreateMap<ProductDetail, CreateProductDetailDto>().ReverseMap();
+            CreateMap<ProductDetail, UpdateProductDetailDto>().ReverseMap();
+            CreateMap<ProductDetail, GetByIdProductDetailDto>().ReverseMap();
+
+            CreateMap<ProductImage, ResultProductImageDto>().ReverseMap();
+            CreateMap<ProductImage, CreateProductImageDto>().ReverseMap();
+            CreateMap<ProductImage, UpdateProductImageDto>().ReverseMap();
+            CreateMap<ProductImage, GetByIdProductImageDto>().ReverseMap();
+
+        }
+    }
+}
---
+++ Services/Catalog/MDShop.Catalog/Services/CategoryServices/CategoryService.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MDShop.Catalog.Dtos.CategoryDtos;
+using MDShop.Catalog.Entities;
+using MongoDB.Driver;
+
+namespace MDShop.Catalog.Services.CategoryServices {
+    public class CategoryService : ICategoryService {
+        private readonly IMongoCollection<Category> _categoryCollection;
+        private readonly IMapper _mapper;
+        public Task CreateCategoryAsync(CreateCategoryDto createCategoryDto) {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultCategoryDto>> GetAllCategoryAsync() {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetByIdCategoryDto> GetByIdCategoryAsync() {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCategoryAsync(string id) {
+            throw new NotImplementedException();
+        }
+    }
+}
