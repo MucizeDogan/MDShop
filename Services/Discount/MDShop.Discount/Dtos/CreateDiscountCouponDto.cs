921469a3ed585a0010d6a5f05c045736a0d1e8e4
M-racle <108946729+M-racle@users.noreply.github.com>
2025-01-12 18:11:00 +0300

    Discount service CRUD operations were written. Coupon class DTOs were created.

---
+++ Services/Discount/MDShop.Discount/Dtos/CreateDiscountCouponDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MDShop.Discount.Dtos {
+    public class CreateDiscountCouponDto {
+        public string Code { get; set; }
+        public string Rate { get; set; } // Kuponun indirim oranı
+        public bool IsActive { get; set; }
+        public DateTime ValidDate { get; set; }
+    }
+}
---
+++ Services/Discount/MDShop.Discount/Services/IDiscountService.cs
@@ -0,0 +1,11 @@
+﻿using MDShop.Discount.Dtos;
+
+namespace MDShop.Discount.Services {
+    public interface IDiscountService {
+        Task<List<ResultDiscountCouponDto>> GetAllDiscountCouponAsync();
+        Task CreateDiscountCouponAsync(CreateDiscountCouponDto createCouponDto);
+        Task UpdateDiscountCouponAsync(UpdateDiscountCouponDto updateCouponDto);
+        Task DeleteDiscountCouponAsync(int id);
+        Task<GetByIdDiscountCouponDto> GetByIdDiscountCouponAsync(int id);
+    }
+}
