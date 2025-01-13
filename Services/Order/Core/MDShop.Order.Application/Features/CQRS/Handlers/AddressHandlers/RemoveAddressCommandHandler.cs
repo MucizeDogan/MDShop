ac86107632c880d45deb314560e2f9cccecdaa40
M-racle <108946729+M-racle@users.noreply.github.com>
2025-01-13 15:38:35 +0300

    Remove, update, get Handlers were written for CQRS address.

---
+++ Services/Order/Core/MDShop.Order.Application/Features/CQRS/Handlers/AddressHandlers/RemoveAddressCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using MDShop.Order.Application.Features.CQRS.Commands.AddressCommands;
+using MDShop.Order.Application.Interfaces;
+using MDShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDShop.Order.Application.Features.CQRS.Handlers.AddressHandlers {
+    public class RemoveAddressCommandHandler {
+        private readonly IRepository<Address> _repository;
+
+        public RemoveAddressCommandHandler(IRepository<Address> repository) {
+            _repository = repository;
+        }
+
+        public async Task Handle(RemoveAddressCommand command) {
+            var value = await _repository.GetByIdAsync(command.Id);
+            await _repository.DeleteAsync(value);
+        }
+    }
+}
