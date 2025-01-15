6b055d4f3446135441fb49ff9eeb72390238b6c9
M-racle <108946729+M-racle@users.noreply.github.com>
2025-01-15 13:55:26 +0300

    Writing Address and Order Detail APIs and registration processes in program.cs

--- Services/Order/Presentation/MDShop.Order.WebApi/Program.cs
+++ Services/Order/Presentation/MDShop.Order.WebApi/Program.cs
@@ -1,6 +1,23 @@
+using MDShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
+using MDShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+#region Address & OrderDetail CQRS Registration
+builder.Services.AddScoped<GetAddressQueryHandler>();
+builder.Services.AddScoped<GetAddressByIdQueryHandler>();
+builder.Services.AddScoped<CreateAddressCommandHandler>();
+builder.Services.AddScoped<UpdateAddressCommandHandler>();
+builder.Services.AddScoped<RemoveAddressCommandHandler>();
+
+builder.Services.AddScoped<GetOrderDetailQueryHandler>();
+builder.Services.AddScoped<GetOrderDetailByIdQueryHandler>();
+builder.Services.AddScoped<CreateOrderDetailCommandHandler>();
+builder.Services.AddScoped<UpdateOrderDetailCommandHandler>();
+builder.Services.AddScoped<RemoveOrderDetailCommandHandler>();
+#endregion
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
