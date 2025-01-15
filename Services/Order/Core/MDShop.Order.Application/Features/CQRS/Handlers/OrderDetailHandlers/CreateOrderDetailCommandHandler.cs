7109b3edc2e0cbcf9008e478ffef8dee18f6b50f
M-racle <108946729+M-racle@users.noreply.github.com>
2025-01-15 11:22:15 +0300

    CQRS operations were performed for the Order Detail entity.

---
+++ Services/Order/Core/MDShop.Order.Application/Features/CQRS/Handlers/OrderDetailHandlers/CreateOrderDetailCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MDShop.Order.Application.Features.CQRS.Commands.OrderDetailCommands;
+using MDShop.Order.Application.Interfaces;
+using MDShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers {
+    public class CreateOrderDetailCommandHandler {
+        private readonly IRepository<OrderDetail> _repository;
+
+        public CreateOrderDetailCommandHandler(IRepository<OrderDetail> repository) {
+            _repository = repository;
+        }
+
+        public async Task Handle(CreateOrderDetailCommand command) {
+            await _repository.CreateAsync(new OrderDetail {
+                ProductAmount = command.ProductAmount,
+                OrderingId = command.OrderingId,
+                ProductId = command.ProductId,
+                ProductName = command.ProductName,
+                ProductPrice = command.ProductPrice,
+                ProductTotalPrice = command.ProductTotalPrice
+            });
+        }
+    }
+}
