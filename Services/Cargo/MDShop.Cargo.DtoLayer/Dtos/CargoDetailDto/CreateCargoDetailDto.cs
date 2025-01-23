9f6052e2da1fc9a23b5421b8386c849ca9c784c2
M-racle <108946729+M-racle@users.noreply.github.com>
2025-01-23 10:21:23 +0300

    Cargo Company, Cargo Detail and Cargo Customer controllers were created. DTOs were created.

---
+++ Services/Cargo/MDShop.Cargo.DtoLayer/Dtos/CargoDetailDto/CreateCargoDetailDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDShop.Cargo.DtoLayer.Dtos.CargoDetailDto {
+    public class CreateCargoDetailDto {
+        public string SenderCustomer { get; set; }
+        public string ReceiverCustomer { get; set; }
+        public int Barcode { get; set; }
+        public int CargoCompanyId { get; set; }
+    }
+}
---
+++ Services/Cargo/MDShop.Cargo.DtoLayer/Dtos/CargoDetailDto/UpdateCargoDetailDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDShop.Cargo.DtoLayer.Dtos.CargoDetailDto {
+    public class UpdateCargoDetailDto {
+        public int CargoDetailId { get; set; }
+        public string SenderCustomer { get; set; }
+        public string ReceiverCustomer { get; set; }
+        public int Barcode { get; set; }
+        public int CargoCompanyId { get; set; }
+    }
+}
