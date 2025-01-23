855ffea9c26231c977716b76741d2cf79d8bf497
M-racle <108946729+M-racle@users.noreply.github.com>
2025-01-23 09:41:18 +0300

    For our Cargo service, we filled the Data Access and Business layers with our interfaces and classes in accordance with the n-tier architecture.

---
+++ Services/Cargo/MDShop.Cargo.BusinessLayer/Concrete/CargoCustomerManager.cs
@@ -0,0 +1,32 @@
+﻿using MDShop.Cargo.BusinessLayer.Abstract;
+using MDShop.Cargo.DataAccessLayer.Abstract;
+using MDShop.Cargo.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDShop.Cargo.BusinessLayer.Concrete {
+    public class CargoCustomerManager : ICargoCustomerService {
+        private readonly ICargoCustomerDal _cargoCustomerDal;
+        public CargoCustomerManager(ICargoCustomerDal cargoCustomerDal) {
+            _cargoCustomerDal = cargoCustomerDal;
+        }
+        public void TDelete(int id) {
+            _cargoCustomerDal.Delete(id);
+        }
+        public List<CargoCustomer> TGetAll() {
+            return _cargoCustomerDal.GetAll();
+        }
+        public CargoCustomer TGetById(int id) {
+            return _cargoCustomerDal.GetById(id);
+        }
+        public void TInsert(CargoCustomer entity) {
+            _cargoCustomerDal.Insert(entity);
+        }
+        public void TUpdate(CargoCustomer entity) {
+            _cargoCustomerDal.Update(entity);
+        }
+    }
+}
