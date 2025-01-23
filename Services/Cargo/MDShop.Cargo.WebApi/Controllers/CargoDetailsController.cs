75a0e844c0421fc6e8fe71bbe4bb8e76ca3d62da
M-racle <108946729+M-racle@users.noreply.github.com>
2025-01-23 11:12:39 +0300

    Controller and DTOs were created for CargoOperation. Cargo Microservice was protected with Identity Server. Api was tested.

--- Services/Cargo/MDShop.Cargo.WebApi/Controllers/CargoDetailsController.cs
+++ Services/Cargo/MDShop.Cargo.WebApi/Controllers/CargoDetailsController.cs
@@ -1,10 +1,12 @@
 ﻿using MDShop.Cargo.BusinessLayer.Abstract;
 using MDShop.Cargo.DtoLayer.Dtos.CargoDetailDto;
 using MDShop.Cargo.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MDShop.Cargo.WebApi.Controllers {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class CargoDetailsController : ControllerBase {
--- Services/Cargo/MDShop.Cargo.WebApi/Program.cs
+++ Services/Cargo/MDShop.Cargo.WebApi/Program.cs
@@ -1,6 +1,29 @@
+using MDShop.Cargo.BusinessLayer.Abstract;
+using MDShop.Cargo.BusinessLayer.Concrete;
+using MDShop.Cargo.DataAccessLayer.Abstract;
+using MDShop.Cargo.DataAccessLayer.Concrete;
+using MDShop.Cargo.DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt => {
+    opt.Authority = builder.Configuration["IdentityServerUrl"];
+    opt.Audience = "ResourceCargo";
+    opt.RequireHttpsMetadata = false;
+});
+
+builder.Services.AddDbContext<CargoContext>();
+builder.Services.AddScoped<ICargoCompanyDal, EfCargoCompanyDal>();
+builder.Services.AddScoped<ICargoCompanyService, CargoCompanyManager>();
+builder.Services.AddScoped<ICargoCustomerDal, EfCargoCustomerDal>();
+builder.Services.AddScoped<ICargoCustomerService, CargoCustomerManager>();
+builder.Services.AddScoped<ICargoDetailDal, EfCargoDetailDal>();
+builder.Services.AddScoped<ICargoDetailService, CargoDetailManager>();
+builder.Services.AddScoped<ICargoOperationDal, EfCargoOperationDal>();
+builder.Services.AddScoped<ICargoOperationService, CargoOperationManager>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -17,6 +40,7 @@ if (app.Environment.IsDevelopment()) {
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
