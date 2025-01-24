3baaaf485fdfaca797abfe2c838d4efc1c944474
M-racle <108946729+M-racle@users.noreply.github.com>
2025-01-24 12:16:51 +0300

    Basket services and Controller operations were performed.

---
+++ Services/Basket/MDShop.Basket/LoginServices/ILoginService.cs
@@ -0,0 +1,5 @@
+﻿namespace MDShop.Basket.LoginServices {
+    public interface ILoginService {
+        public string GetUserId { get; } //set yok sadece okuma olacağı için
+    }
+}
---
+++ Services/Basket/MDShop.Basket/Services/IBasketService.cs
@@ -0,0 +1,10 @@
+﻿using MDShop.Basket.Dtos;
+
+namespace MDShop.Basket.Services {
+    public interface IBasketService {
+        Task<BasketTotalDto> GetBasket(string userId);
+        Task SaveBasket(BasketTotalDto basketTotalDto);
+        Task DeleteBasket(string userId);
+
+    }
+}
