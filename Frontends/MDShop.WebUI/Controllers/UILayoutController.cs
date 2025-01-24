b8ab8c13e78cae8b962c92eca72e66b3eb02733b
M-racle <108946729+M-racle@users.noreply.github.com>
2025-01-24 17:13:24 +0300

    Template file added to the project. UI Layout Controller and view created for layout. Preparation made for layout.

---
+++ Frontends/MDShop.WebUI/Controllers/UILayoutController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MDShop.WebUI.Controllers {
+    public class UILayoutController : Controller {
+        public IActionResult _UILayout() {
+            return View();
+        }
+    }
+}
