867cc01918802729a953d8d7b71ae2fdef17c1c7
M-racle <108946729+M-racle@users.noreply.github.com>
2025-01-25 19:15:02 +0300

    Contiuning HomePage Component Partial

---
+++ Frontends/MDShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MDShop.WebUI.ViewComponents.DefaultViewComponents {
+    public class _FeatureProductsDefaultComponentPartial : ViewComponent {
+        public IViewComponentResult Invoke() {
+            return View();
+        }
+    }
+}
