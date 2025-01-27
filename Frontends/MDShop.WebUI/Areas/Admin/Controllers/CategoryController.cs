f8897336f53b147874c8f3f195577d35734361b6
M-racle <108946729+M-racle@users.noreply.github.com>
2025-01-27 14:44:14 +0300

    Area added for Admin Panel. We separate View Components for AdminLayout.

---
+++ Frontends/MDShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MDShop.WebUI.Areas.Admin.Controllers {
+    public class CategoryController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
