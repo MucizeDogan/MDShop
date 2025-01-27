142f6dcfef708187cbbe944fe42eaa5bf88a32ac
M-racle <108946729+M-racle@users.noreply.github.com>
2025-01-27 16:19:22 +0300

    Necessary preparations have been made for the listing of categories.

---
+++ Frontends/MDShop.WebUI/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutPageHeaderComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MDShop.WebUI.Areas.Admin.ViewComponents.AdminLayoutViewComponents {
+    public class _AdminLayoutPageHeaderComponentPartial : ViewComponent {
+        public IViewComponentResult Invoke() {
+            return View();
+        }
+    }
+}
