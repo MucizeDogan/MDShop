91c2a7837f4e40259504203e4c5fa9afbd2e01be
M-racle <108946729+M-racle@users.noreply.github.com>
2025-01-27 13:37:54 +0300

    Added Contact and Shopping cart controller and pages

---
+++ Frontends/MDShop.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MDShop.WebUI.Controllers {
+    public class ContactController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
