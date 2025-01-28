6ef26aaf4b1cd3d24f7d261aecf0e583fc378c28
M-racle <108946729+M-racle@users.noreply.github.com>
2025-01-28 19:34:53 +0300

    Admin area created for admin. Controller created for category operations.

--- Frontends/MDShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ Frontends/MDShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -1,13 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MDShop.DtoLayer.CatalogDtos.CategoryDto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace MDShop.WebUI.Areas.Admin.Controllers {
     [Area("Admin")]
     public class CategoryController : Controller {
-        public IActionResult Index() {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoryController(IHttpClientFactory httpClientFactory) {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> Index() {
             ViewBag.v0 = "Kategori İşlemleri";
             ViewBag.v1 = "Ana Sayfa";
             ViewBag.v2 = "Kategoriler";
             ViewBag.v3 = "Kategori Listesi";
+
+            var client = _httpClientFactory.CreateClient();
+            var res = await client.GetAsync("https://localhost:7070/api/Categories"); //isteğin yapılacağı adres (port-catalog servisine gidecek)
+            if (res.IsSuccessStatusCode) {
+                var jsonData = await res.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+
             return View();
         }
     }
