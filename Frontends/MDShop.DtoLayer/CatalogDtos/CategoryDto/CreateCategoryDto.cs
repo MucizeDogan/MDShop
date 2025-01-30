9206abddacff14723a54235ce96378dce64cf7e3
M-racle <108946729+M-racle@users.noreply.github.com>
2025-01-30 10:45:43 +0300

    Listing, adding new categories and deleting operations have been made possible on the admin side. Both the controller and view sides have been made.

---
+++ Frontends/MDShop.DtoLayer/CatalogDtos/CategoryDto/CreateCategoryDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDShop.DtoLayer.CatalogDtos.CategoryDto {
+    public class CreateCategoryDto {
+        public string CategoryName { get; set; }
+    }
+}
