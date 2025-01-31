90ff469fe91594f059469cfe35b0ace6bbabe276
M-racle <108946729+M-racle@users.noreply.github.com>
2025-01-31 16:10:04 +0300

    Started to make the Home Page dynamic. FeatureSlider entity, Dto, Service and database operations were done.

---
+++ Services/Catalog/MDShop.Catalog/Entities/FeatureSlider.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MDShop.Catalog.Entities {
+    public class FeatureSlider {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)] //Benzersiz olduğunu bildirmek için parametre
+        public string FeatureSliderId { get; set; } // MongoDB de id ler string guid değer aldığı için string yaptık. Ayrıca id olduğunu belirtmek için 2 tane attirubute var
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ Services/Catalog/MDShop.Catalog/Services/FeatureSliderServices/IFeatureSliderService.cs
@@ -0,0 +1,13 @@
+﻿using MDShop.Catalog.Dtos.FeatureSliderDtos;
+
+namespace MDShop.Catalog.Services.FeatureSliderServices {
+    public interface IFeatureSliderService {
+        Task<List<ResultFeatureSliderDto>> GetAllFeatureSliderAsync();
+        Task CreateFeatureSliderAsync(CreateFeatureSliderDto createFeatureSliderDto);
+        Task UpdateFeatureSliderAsync(UpdateFeatureSliderDto updateFeatureSliderDto);
+        Task DeleteFeatureSliderAsync(string id);
+        Task<GetByIdFeatureSliderDto> GetByIdFeatureSliderAsync(string id);
+        Task FeatureSliderChangeStatusToTrue(string id);
+        Task FeatureSliderChangeStatusToFalse(string id);
+    }
+}
