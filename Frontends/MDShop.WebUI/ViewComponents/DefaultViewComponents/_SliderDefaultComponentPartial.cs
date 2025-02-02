560f10c4f56a7ca9aeb605f68f735a94e4c0e561
M-racle <108946729+M-racle@users.noreply.github.com>
2025-02-02 11:19:21 +0300

    ..

--- Frontends/MDShop.WebUI/ViewComponents/DefaultViewComponents/_SliderDefaultComponentPartial.cs
+++ Frontends/MDShop.WebUI/ViewComponents/DefaultViewComponents/_SliderDefaultComponentPartial.cs
@@ -1,8 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MDShop.DtoLayer.CatalogDtos.FeatureSliderDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace MDShop.WebUI.ViewComponents.DefaultViewComponents {
     public class _SliderDefaultComponentPartial : ViewComponent{
-        public IViewComponentResult Invoke() {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _SliderDefaultComponentPartial(IHttpClientFactory httpClientFactory) {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() {
+            var client = _httpClientFactory.CreateClient();
+            var res = await client.GetAsync("https://localhost:7070/api/FeatureSliders"); //isteğin yapılacağı adres (port-catalog servisine gidecek)
+            if (res.IsSuccessStatusCode) {
+                var jsonData = await res.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureSliderDto>>(jsonData);
+                var trueValues = values.Where(x => x.Status == true).ToList();
+                return View(trueValues);
+            }
+
             return View();
         }
     }
