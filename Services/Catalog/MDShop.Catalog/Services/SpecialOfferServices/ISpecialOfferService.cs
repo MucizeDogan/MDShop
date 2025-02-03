bdaca56527541399fc556b5319f893d052487e0f
M-racle <108946729+M-racle@users.noreply.github.com>
2025-02-03 17:51:17 +0300

    New entity, dtos, services and database settings have been made for the special offers area on the Home Page.

---
+++ Services/Catalog/MDShop.Catalog/Services/SpecialOfferServices/ISpecialOfferService.cs
@@ -0,0 +1,13 @@
+﻿using MDShop.Catalog.Dtos.SpecialOfferDtos;
+
+namespace MDShop.Catalog.Services.SpecialOfferServices {
+    public interface ISpecialOfferService {
+        Task<List<ResultSpecialOfferDto>> GetAllSpecialOfferAsync();
+        Task CreateSpecialOfferAsync(CreateSpecialOfferDto createSpecialOfferDto);
+        Task UpdateSpecialOfferAsync(UpdateSpecialOfferDto updateSpecialOfferDto);
+        Task DeleteSpecialOfferAsync(string id);
+        Task<GetByIdSpecialOfferDto> GetByIdSpecialOfferAsync(string id);
+        Task SpecialOfferChangeStatusToTrue(string id);
+        Task SpecialOfferChangeStatusToFalse(string id);
+    }
+}
---
+++ Services/Catalog/MDShop.Catalog/Services/SpecialOfferServices/SpecialOfferService.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using MDShop.Catalog.Dtos.FeatureSliderDtos;
+using MDShop.Catalog.Dtos.SpecialOfferDtos;
+using MDShop.Catalog.Entities;
+using MDShop.Catalog.Settings;
+using Microsoft.VisualBasic;
+using MongoDB.Driver;
+
+namespace MDShop.Catalog.Services.SpecialOfferServices {
+    public class SpecialOfferService : ISpecialOfferService {
+        private readonly IMongoCollection<SpecialOffer> _specialOfferCollection;
+        private readonly IMapper _mapper;
+        public SpecialOfferService(IMapper mapper, IDatabaseSettings _databaseSettings) {
+            var client = new MongoClient(_databaseSettings.ConnectionString);
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);
+            _specialOfferCollection = database.GetCollection<SpecialOffer>(_databaseSettings.SpecialOfferCollectionName);
+            _mapper = mapper;
+        }
+
+        public async Task<List<ResultSpecialOfferDto>> GetAllSpecialOfferAsync() {
+            //var values = await _specialOfferCollection.Find(x => true).ToListAsync();
+            //return _mapper.Map<List<ResultSpecialOfferDto>>(values);
+
+            var values = await _specialOfferCollection
+                .Find(x => x.Status == true)  // Status true olanları getir
+                .SortBy(x => x.Order)         // Order alanına göre sırala
+                .ToListAsync();
+            return _mapper.Map<List<ResultSpecialOfferDto>>(values);
+        }
+
+        public async Task CreateSpecialOfferAsync(CreateSpecialOfferDto createSpecialOfferDto) {
+            var value = _mapper.Map<SpecialOffer>(createSpecialOfferDto);
+            await _specialOfferCollection.InsertOneAsync(value);
+        }
+
+        public async Task DeleteSpecialOfferAsync(string id) {
+            await _specialOfferCollection.DeleteOneAsync(x => x.SpecialOfferId == id);
+        }
+
+        public async Task UpdateSpecialOfferAsync(UpdateSpecialOfferDto updateSpecialOfferDto) {
+            var values = _mapper.Map<SpecialOffer>(updateSpecialOfferDto);
+            await _specialOfferCollection.FindOneAndReplaceAsync(x => x.SpecialOfferId == updateSpecialOfferDto.SpecialOfferId, values);
+        }
+
+        public async Task<GetByIdSpecialOfferDto> GetByIdSpecialOfferAsync(string id) {
+            var values = await _specialOfferCollection.Find<SpecialOffer>(x => x.SpecialOfferId == id).FirstOrDefaultAsync();
+            return _mapper.Map<GetByIdSpecialOfferDto>(values);
+        }
+
+        public async Task SpecialOfferChangeStatusToTrue(string id) {
+            var value = await _specialOfferCollection.Find<SpecialOffer>(x => x.SpecialOfferId == id).FirstOrDefaultAsync();
+            value.Status = true;
+            await _specialOfferCollection.ReplaceOneAsync(x => x.SpecialOfferId == id, value);
+        }
+
+        public async Task SpecialOfferChangeStatusToFalse(string id) {
+            var value = await _specialOfferCollection.Find<SpecialOffer>(x => x.SpecialOfferId == id).FirstOrDefaultAsync();
+            value.Status = false;
+            await _specialOfferCollection.ReplaceOneAsync(x => x.SpecialOfferId == id, value);
+        }
+    }
+}
