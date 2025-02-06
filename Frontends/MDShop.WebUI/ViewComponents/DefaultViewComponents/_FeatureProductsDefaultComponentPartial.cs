bcc8b7f369b806e7364d67c835a279e91740e536
M-racle <108946729+M-racle@users.noreply.github.com>
2025-02-06 15:36:40 +0300

    ..

--- Frontends/MDShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
+++ Frontends/MDShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
@@ -1,8 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MDShop.DtoLayer.CatalogDtos.ProductDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace MDShop.WebUI.ViewComponents.DefaultViewComponents {
     public class _FeatureProductsDefaultComponentPartial : ViewComponent {
-        public IViewComponentResult Invoke() {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FeatureProductsDefaultComponentPartial(IHttpClientFactory httpClientFactory) {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() {
+            var client = _httpClientFactory.CreateClient();
+            var res = await client.GetAsync("https://localhost:7070/api/Products"); //isteğin yapılacağı adres (port-catalog servisine gidecek)
+            if (res.IsSuccessStatusCode) {
+                var jsonData = await res.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(values);
+            }
+
             return View();
         }
     }
