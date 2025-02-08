8f7b2fb217aa702e12e4aface27c26b8c8a4e752
M-racle <108946729+M-racle@users.noreply.github.com>
2025-02-08 21:18:25 +0300

    It is now possible to go to the product detail page from the latest products added to the Home Page.

--- Frontends/MDShop.WebUI/Controllers/ProductListController.cs
+++ Frontends/MDShop.WebUI/Controllers/ProductListController.cs
@@ -7,7 +7,8 @@ namespace MDShop.WebUI.Controllers {
             return View();
         }
 
-        public IActionResult ProductDetail() {
+        public IActionResult ProductDetail(string id) {
+            ViewBag.x = id;
             return View();
         }
     }
