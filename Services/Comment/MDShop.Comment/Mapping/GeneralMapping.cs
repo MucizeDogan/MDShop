b127a62f2638fb2c20c445b239942543d105feec
M-racle <108946729+M-racle@users.noreply.github.com>
2025-02-10 16:09:26 +0300

    Added and mapped dtos for comments.

---
+++ Services/Comment/MDShop.Comment/Mapping/GeneralMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using MDShop.Comment.Dtos.CommentDtos;
+using MDShop.Comment.Entities;
+
+namespace MDShop.Comment.Mapping {
+    public class GeneralMapping : Profile {
+        public GeneralMapping() {
+
+            CreateMap<UserComment, ResultCommentDto>().ReverseMap();
+            CreateMap<UserComment, CreateCommentDto>().ReverseMap();
+            CreateMap<UserComment, UpdateCommentDto>().ReverseMap();
+            CreateMap<UserComment, GetByIdCommentDto>().ReverseMap();
+
+        }
+    }
+}
