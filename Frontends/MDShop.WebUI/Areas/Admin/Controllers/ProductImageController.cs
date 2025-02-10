f0831d3f1deef431e9fa79325cb246d1e627abcc
M-racle <108946729+M-racle@users.noreply.github.com>
2025-02-10 11:12:04 +0300

    A plugin was made to update product images on the admin side.

--- Frontends/MDShop.WebUI/Areas/Admin/Controllers/ProductImageController.cs
+++ Frontends/MDShop.WebUI/Areas/Admin/Controllers/ProductImageController.cs
@@ -1,8 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MDShop.DtoLayer.CatalogDtos.ProductImageDtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace MDShop.WebUI.Areas.Admin.Controllers {
+    [Area("Admin")]
+    [AllowAnonymous]
+    [Route("Admin/ProductImage")]
     public class ProductImageController : Controller {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductImageController(IHttpClientFactory httpClientFactory) {
+            _httpClientFactory = httpClientFactory;
+        }
+        [Route("ProductImageDetail/{id}")]
+        [HttpGet]
         public async Task<IActionResult> ProductImageDetail(string id) {
+            var client = _httpClientFactory.CreateClient();
+            var res = await client.GetAsync("https://localhost:7070/api/ProductImages/ProductImagesByProductId?id=" + id);
+            if (res.IsSuccessStatusCode) {
+                var jsonData = await res.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateProductImageDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [Route("ProductImageDetail/{id}")]
+        [HttpPost]
+        public async Task<IActionResult> ProductImageDetail(UpdateProductImageDto updateProductImageDto) {
+            var client = _httpClientFactory.CreateClient();
+            
+            var jsonData = JsonConvert.SerializeObject(updateProductImageDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var res = await client.PutAsync("https://localhost:7070/api/ProductImages", stringContent);
+            if (res.IsSuccessStatusCode) {
+                return RedirectToAction("ProductListWithCategory", "Product", new { area = "Admin" });
+            }
             return View();
         }
     }
