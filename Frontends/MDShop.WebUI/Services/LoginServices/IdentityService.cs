a09287ba313ac7c30060cf5d6f7e031999af4bb8
M-racle <108946729+M-racle@users.noreply.github.com>
2025-02-13 11:26:19 +0300

    By communicating with our Identity Server layer and WebUI layer, operations are performed to obtain information such as role information etc. via token.

---
+++ Frontends/MDShop.WebUI/Services/LoginServices/IdentityService.cs
@@ -0,0 +1,9 @@
+﻿using MDShop.DtoLayer.IdentityDtos.LoginDtos;
+
+namespace MDShop.WebUI.Services.LoginServices {
+    public class IdentityService : IIdentityService {
+        public Task<bool> SignIn(SignUpDto signUpDto) {
+            
+        }
+    }
+}
