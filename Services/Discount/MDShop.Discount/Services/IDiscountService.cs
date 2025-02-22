2dc1decda92b383d35d986ae7b3b42ccc41f01ba
M-racle <108946729+M-racle@users.noreply.github.com>
2025-02-22 19:37:49 +0300

    ...

--- Services/Discount/MDShop.Discount/Services/IDiscountService.cs
+++ Services/Discount/MDShop.Discount/Services/IDiscountService.cs
@@ -7,5 +7,6 @@ namespace MDShop.Discount.Services {
         Task UpdateDiscountCouponAsync(UpdateDiscountCouponDto updateCouponDto);
         Task DeleteDiscountCouponAsync(int id);
         Task<GetByIdDiscountCouponDto> GetByIdDiscountCouponAsync(int id);
+        Task<ResultDiscountCouponDto> GetCodeDetailByCodeAsync(string code);
     }
 }
